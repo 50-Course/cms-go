b6ddda28fa62ed3359a99cc904b2783834a749b4
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-05-03 04:04:01 +0100

    refactor(content-metadata): extract related fields into `BaseMetadata` struct

--- content/content.go
+++ content/content.go
@@ -2,31 +2,36 @@ package content
 
 import _ "github.com/golang/protobuf/ptypes/timestamp"
 
+// Base Metadata
+//
+// Contains shared data relatable across all content types
+type BaseMetadata struct {
+	ID    int64  `json:"id"`
+	Title string `json:"title"`
+}
+
 // ImageContent is a content type that represents an image contentj
 type ImageContent struct {
-	Author  string `json:"author"`
-	Url     string `json:"url"`
-	ID      int64  `json:"id"`
-	Title   string `json:"title"`
-	AltText string `json:"alttext"`
+	Author   string       `json:"author"`
+	Url      string       `json:"url"`
+	AltText  string       `json:"alttext"`
+	Metadata BaseMetadata `json:"metadata"`
 }
 
 // VideoContent is a content type that represents a video content
 type VideoContent struct {
-	Author string `json:"author"`
-	Url    string `json:"url"`
-	ID     int64  `json:"id"`
-	Title  string `json:"title"`
-	Format string `json:"format"`
+	Author   string       `json:"author"`
+	Url      string       `json:"url"`
+	Format   string       `json:"format"`
+	Metadata BaseMetadata `json:"metadata"`
 }
 
 // TextContent is a content type that represents a text content
 type TextContent struct {
-	Author      string `json:"author"`
-	Url         string `json:"url"`
-	ID          int64  `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"alttext"`
+	Author      string       `json:"author"`
+	Url         string       `json:"url"`
+	Description string       `json:"alttext"`
+	Metadata    BaseMetadata `json:"metadata"`
 }
 
 // Author is essentually a user in the system that access to publish or moderate
